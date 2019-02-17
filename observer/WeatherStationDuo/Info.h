72a71a220622158f16dba2d9ef0fd6b74f9af0c9
Федякова Анастасия <fedyakova.anastasia@gmail.com>
2019-02-18 02:24:27 +0300

    Добавила наблюдение за несколькими субъектами.

---
+++ observer/WeatherStationDuo/Info.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <climits>
+#include <iostream>
+
+using namespace std;
+
+class CInfo
+{
+public:
+	void UpdateInfo(double indicator)
+	{
+		if (indicator < m_minIndicator)
+		{
+			m_minIndicator = indicator;
+		}
+
+		if (m_maxIndicator < indicator)
+		{
+			m_maxIndicator = indicator;
+		}
+
+		m_accIndicator += indicator;
+		++m_countAcc;
+
+		std::cout << "Max indicator " << m_maxIndicator << std::endl;
+		std::cout << "Min indicator " << m_minIndicator << std::endl;
+		std::cout << "Average indicator " << m_accIndicator / m_countAcc << std::endl;
+		std::cout << "----------------" << std::endl;
+	}
+
+private:
+	double m_minIndicator = std::numeric_limits<double>::infinity();
+	double m_maxIndicator = -std::numeric_limits<double>::infinity();
+	double m_accIndicator = 0;
+	unsigned m_countAcc = 0;
+};
