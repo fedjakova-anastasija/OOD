958eff9d70ddd494b0d654b0e9c376af854b1e6b
Федякова Анастасия <fedyakova.anastasia@gmail.com>
2019-05-12 19:17:29 +0300

    Исправила директорию на временную.

--- command/iSpringWord/ISpringWordTests/ConstPath.h
+++ command/iSpringWord/ISpringWordTests/ConstPath.h
@@ -3,7 +3,7 @@
 
 const std::string IMAGE = "image.png";
 const std::string HTML_FILE = "test.html";
-const std::string PATH = boost::filesystem::current_path().string() + "/";
+const std::string PATH = boost::filesystem::temp_directory_path().string() + "/";
 
 const std::string IMAGE_PATH = PATH + IMAGE;
 const std::string FILE_PATH = PATH + HTML_FILE;
